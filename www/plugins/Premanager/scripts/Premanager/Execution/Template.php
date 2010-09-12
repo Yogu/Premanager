20f95c784692fec2d15429855efe2357fb40160c
Yogu <info@yogularm.de>
2010-09-12 21:48:59 +0200

    Premanager\Execution\Template provides access to the Dwoo template engine
    The Template class is linked to a template file in the "template" folder of a plugin with the file extension ".tpl".
    
    The set() method can be used to assign data to the collection and clear() to remove all those data. get() renders the template using the provided data and returns the result.
    
    By default, the output is compressed. This means that all unnecessary whitespaces are removed to make the file, e.g. a html or css file, as small as possible. This feature can be disabled for a specific template and is turned off completely in debug mode.

--- www/plugins/Premanager/scripts/Premanager/Execution/Template.php
+++ www/plugins/Premanager/scripts/Premanager/Execution/Template.php
@@ -1,11 +1,111 @@
 <?php
 namespace Premanage\Execution;
 
+use Premanager\Module;
+use Premanager\IO\FileNotFoundException;
+use Premanager\ArgumentException;
+use Premanager\IO\File;
+use Premanager\IO\Config;
+
 /**
- * A template with values
+ * A template with data
  */
-class Template {
+class Template extends Module {
+	/**
+	 * @var Dwoo
+	 */
+	private $_dwoo;
+	/**
+	 * @var Dwoo_Data
+	 */
+	private $_data;
+	/**
+	 * @var string
+	 */
+	private $_path;
+	/**
+	 * @var Dwoo_Compiler
+	 */
+	private $_compiler;
 	
+	/**
+	 * @var Dwoo_Compiler
+	 */
+	private static $_defaultCompiler;
+	/**
+	 * @var Dwoo_Compiler
+	 */
+	private static $_compilerWithoutCompressor;
+	
+	/**
+	 * Creates a template with the file
+	 * /plugins/{pluginName}/templates/{templateName}.tpl
+	 * 
+	 * @param string $pluginName the name of the plugin that stores the template
+	 * @param string $templateName the file name without extension 
+	 * @param bool $disableCompressor true, if the output should not be compressed
+	 * @throws FileNotFoundException the file given by $pluginName and
+	 *   $templateName does not exist
+	 */
+	public function __construct($pluginName, $templateName,
+		$disableCompressor = false) {
+		parent::__construct();
+		
+		$this->_path = Config::getPluginPath().'/'.$pluginName.'/templates/'.
+			$templateName.'.tpl';
+		if (File::exists($this->_path))
+			throw new FileNotFoundException('The template file does not exist',
+				$this->_path);
+		
+		require_once(Config::getPluginPath().
+			'/Premanager/thirdparty/dwoo/dwooAutoload.php');
+				
+		$this->_dwoo = new Dwoo();
+		$this->data = new Dwoo_Data();  
+		$this->dwoo->setCompileDir(Config::$cachePath.'Premanager/dwoo');
+		
+		// Select the compiler for this template
+		$enableCompressing = !Config::isDebugMode() && !$disableCompressor;
+		if ($enableCompressing)
+			$this->_compiler =& self::$_defaultCompiler;
+		else
+			$this->_compiler =& self::$_compilerWithoutCompressor;
+			
+		// If that kind of compiler has not been used before, create it
+		if (!$this->_comiler) {
+			$this->_compiler = new Dwoo_Compiler();
+			
+			if ($enableCompressing)
+				$this->_compiler->addPreProcessor('compress', true);
+		}
+	}
+		
+	/**
+	 * Sets a key-value pair of data
+	 * 
+	 * @param string $name the name
+	 * @param string $value the value
+	 */
+	public function set($name, $value) {
+		$this->_data->assign($name, $value);
+	}
+	
+	/**
+	 * Clears all data set by set()
+	 */
+	public function clear() {
+		$this->_data->clear();
+	}
+	
+	/**
+	 * Gets the template rendered with the data given by set()
+	 * 
+	 * @return string the rendered template content
+	 */
+	public function get() {
+		$tpl = new Dwoo_Template_File($this->_path); 
+		return $this->_dwoo->get($tpl, $this->_data, $this->_compiler);
+	}
 }
 
 ?>
