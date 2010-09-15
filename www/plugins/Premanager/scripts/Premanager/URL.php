9e6db2cf28eb2a08e6d9e497eeed3cd0674b629a
Yogu <info@yogularm.de>
2010-09-15 19:56:24 +0200

    Extended the URL class
    Former, the URL class could only separate a url string into host and path.
    
    Now it finds the following url parts: scheme, userinfo, host, port, path, query and fraqument. In another form: scheme://userinfo@host:port/path?query#fragment. The splitting is done using a regular expression based on ABNF from http://tools.ietf.org/html/rfc3986#appendix-A. The full expression is 8 lines long and very confusing, so I have created a slower, but clear version. For performance reasons, a compact form is used, but you find the clear version in a comment.
    
    The URL constructor checks the passed argument instantly and throws a FormatException if it does not match the regular expression.

--- www/plugins/Premanager/scripts/Premanager/URL.php
+++ www/plugins/Premanager/scripts/Premanager/URL.php
@@ -1,6 +1,7 @@
 <?php
 namespace Premanager;
 
+use Premanager\Module;
 use Premanager\Models\Project;
 use Premanager\Models\Language;
 use Premanager\IO\Config;
@@ -9,33 +10,174 @@ use Premanager\Execution\Edition;
 
 /**
  * Provides access to the parts of a url
+ * 
+ * A url can contain following parts:
+ * protocol://user:password@host:port/path?query#fragment
  */
-class URL {
-	private $_url;
+class URL extends Module {
+	private $_originalURL;
+	private $_scheme;
+	private $_userinfo;
 	private $_host;
+	private $_port;
 	private $_path;
+	private $_query;
+	private $_fragment;
+	private $_authority;
+	private $_url;
 	
 	/**
-	 * The host part
+	 * The scheme part (e.g. "http")
+	 * 
+	 * @var string
+	 */
+	public $scheme = Module::PROPERTY_GET;
+	
+	/**
+	 * The userinfo part (e.g. "user:password")
+	 * 
+	 * @var string
+	 */
+	public $userinfo = Module::PROPERTY_GET;
+	
+	/**
+	 * The host part (e.g. "www.example.com")
 	 * 
 	 * @var string
 	 */
 	public $host = Module::PROPERTY_GET;
 	
 	/**
-	 * The path part
+	 * The port part (e.g. "80")
+	 * 
+	 * @var string
+	 */
+	public $port = Module::PROPERTY_GET;
+	
+	/**
+	 * The path part (e.g. "/gallery/index.html")
 	 * 
 	 * @var string
 	 */
 	public $path = Module::PROPERTY_GET;
 	
 	/**
+	 * The query part (e.g. "id=2&mode=show")
+	 * 
+	 * @var string
+	 */
+	public $query = Module::PROPERTY_GET;
+	
+	/**
+	 * The fragment part (e.g. "comments")
+	 * 
+	 * @var string
+	 */
+	public $fragment = Module::PROPERTY_GET;
+	
+	/**
+	 * The authority (e.g. "user:pass@www.example.com:80")
+	 * 
+	 * @var string
+	 */
+	public $authority = Module::PROPERTY_GET;
+	
+	/**
+	 * The path and query (e.g. "/gallery/index.html?id=2&mode=show")
+	 * 
+	 * @var string
+	 */
+	public $pathAndQuery = Module::PROPERTY_GET;
+	
+	/**
+	 * The complete url (e.g.
+	 * "http://user:pass@www.example.com:80/gallery/index.html?id=2&mode=show#comments")
+	 * 
+	 * @var string
+	 */
+	public $url = Module::PROPERTY_GET_ACRONYM;
+	
+	/**
 	 * Creates a new URL using its string representation
 	 * 
 	 * @param string $url the url
+	 * @throws Premanager\FormatException if $url is not a valid url
 	 */
 	public function __construct($url) {
-		$this->_url = $url;
+		$this->_originalURL = $url;
+		
+		/*
+		 * source code for $compiledPattern
+		 * 
+		 * source: ABNF from http://tools.ietf.org/html/rfc3986#appendix-A
+		 * 
+		 * partially translated into a regular expression. For example, ip addresses
+		 * instead of domian names are not supported.
+		
+		$unreserved = '[0-9a-z_\.~\-]';
+		$genDelim = '[\:\/\?\#\[\]\@]';
+		$subDelim = '[\!\$\&\'\(\)\*\+\,\;\=]';
+		$ptcEncoded = '(?:%[a-f0-9])';
+		$pchar = '(?:'.$unreserved.'|'.$subDelim.'|'.$ptcEncoded.'|[\:\@])';
+		
+		$scheme = '(?:(?P<scheme>[a-z][a-z0-9+.-]*)\:\/\/)';
+		$query = '(?:\?(?P<query>(?:'.$pchar.'|[\/\?])*))?';
+		$fragment = '(?:\#(?P<fragment>(?:'.$pchar.'|[\/\?])*))?';
+		
+		$domainPart = '[0-9a-z](?:[0-9a-z-]*[0-9a-z])?';
+		$domain = '(?:'.$domainPart.'\.)+[a-z]{2,}';
+		$host = $domain; // ips not allowed yet
+		$port = '(?:[0-9]+)';
+		$userinfo = '(?:'.$unreserved.'|'.$subDelim.'|'.$ptcEncoded.'|\:)*';
+		$authority = '(?:(?P<userinfo>'.$userinfo.')\@)?(?P<host>'.$host.')'.
+			'(?:\:(?P<port>'.$port.'))?';
+		
+		$segment = '(?:'.$pchar.'*)';
+		$path = '(?P<path>(?:\/'.$segment.'*)*)';
+		
+		$pattern = '/^'.$scheme.$authority.$path.$query.$fragment.'$/i';
+		
+		 *
+		 */
+		
+		// the compiled and optimazed url regular expression pattern
+		$pattern =
+			'/^(?:(?P<scheme>[a-z][a-z0-9+.-]*)\\:\\/\\/)(?:(?P<userinfo>(?:[0-9a-z_'.
+			'\\.~\\-\\!\\$\\&\'\\(\\)\\*\\+\\,\\;\\=]|(?:%[a-f0-9])|\\:)*)\\@)?(?P<h'.
+			'ost>(?:[0-9a-z](?:[0-9a-z-]*[0-9a-z])?\\.)+[a-z]{2,})(?:\\:(?P<port>(?:'.
+			'[0-9]+)))?(?P<path>(?:\\/(?:(?:[0-9a-z_\\.~\\-\\!\\$\\&\'\\(\\)\\*\\+\\'.
+			',\\;\\=\\:\\@]|(?:%[a-f0-9]))*)*)*)(?:\\?(?P<query>(?:(?:[0-9a-z_\\.~\\'.
+			'-\\!\\$\\&\'\\(\\)\\*\\+\\,\\;\\=\\:\\@\\/\\?]|(?:%[a-f0-9])))*))?(?:\\'.
+			'#(?P<fragment>(?:[0-9a-z_\\.~\\-\\!\\$\\&\'\\(\\)\\*\\+\\,\\;\\=\\:\\@'.
+			'\\/\\?]|(?:%[a-f0-9])))*)?$/i';
+		
+		if (!preg_match($pattern, $url, &$matches))
+			throw new FormatException('Invalid url: '.$url);
+		$this->_scheme = (string) $matches['scheme'];
+		$this->_userinfo = (string) $matches['userinfo'];
+		$this->_host = (string) $matches['host'];
+		$this->_port = (string) $matches['port'];
+		$this->_path = $matches['path'] ? $matches['path'] : '/';
+		$this->_query = (string) $matches['query'];
+		$this->_fragment = (string) $matches['fragment'];
+	}
+	
+	/**
+	 * Gets the scheme part
+	 * 
+	 * @return string
+	 */
+	public function getScheme() {
+		return $this->_scheme;
+	}
+	
+	/**
+	 * Gets the userinfo part
+	 * 
+	 * @return string
+	 */
+	public function getUserinfo() {
+		return $this->_userinfo;
 	}
 	
 	/**
@@ -44,9 +186,16 @@ class URL {
 	 * @return string
 	 */
 	public function getHost() {
-		if ($this->_host === null)
-			$this->splitIntoParts();
 		return $this->_host;
+	}
+	
+	/**
+	 * Gets the port part
+	 * 
+	 * @return string
+	 */
+	public function getPort() {
+		return $this->_port;
 	}
 	
 	/**
@@ -55,9 +204,69 @@ class URL {
 	 * @return string
 	 */
 	public function getPath() {
-		if ($this->_path === null)
-			$this->splitIntoParts();
 		return $this->_path;
+	}
+	
+	/**
+	 * Gets the query part
+	 * 
+	 * @return string
+	 */
+	public function getQuery() {
+		return $this->_query;
+	}
+	
+	/**
+	 * Gets the fragment part
+	 * 
+	 * @return string
+	 */
+	public function getFragment() {
+		return $this->_fragment;
+	}
+	
+	/**
+	 * Gets user, password, host and port as string
+	 * 
+	 * @return string
+	 */
+	public function getAuthority() {
+		if ($this->_authority === null) {
+			$this->_authority = '';
+			if ($this->_userinfo)
+				$this->_authority .= $this->_userinfo.'@';
+			$this->_authority .= $this->_host;
+			if ($this->_port)
+				$this->_authority .= ':'.$this->_port;
+		}
+		return $this->_authority;
+	}
+	
+	/**
+	 * Gets the path and query as string
+	 * 
+	 * @return string
+	 */
+	public function getPathAndQuery() {
+		$result = $this->_path;
+		if ($this->_query)
+			$result .= '?' . $this->_query;
+		return $result;
+	}
+	
+	/**
+	 * Gets the complete url
+	 * 
+	 * @return string
+	 */
+	public function getURL() {
+		if ($this->_url === null) {
+			$this->_url = $this->_scheme.'://'.$this->getAuthority().
+				$this->getPathAndQuery();
+			if ($this->_fragment)
+				$this->_url .= '#'.$this->_fragment;
+		}
+		return $this->_url;
 	}
 	
 	/**
@@ -107,17 +316,6 @@ class URL {
 		$template = \str_replace('//', '/', $template);
 		$template = \trim($template, "./").'/';
 		return $template;
-	}
-	
-	private function splitIntoParts() {
-		// Check whether trailing slash after host misses
-		if (\preg_match('!^[a-zA-Z0-9]+:/*[a-zA-Z0-9_.-]*$!', $this->_url))
-			$this->_url .= '/';
-		
-		$expr = '![a-zA-Z0-9]*:/*(?P<host>[a-zA-Z0-9_.-]*)/(?P<path>.*)!';
-		\preg_match($expr, $this->_url, $matches);
-		$this->_host = $matches['host'];                                  
-		$this->_path = $matches['path'];
 	}
 }
 
