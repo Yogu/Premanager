649385f8c998d3e243be3d5b6de634eb3066da08
Yogu <info@yogularm.de>
2010-10-06 20:19:55 +0200

    Created the class TreeListPageNode that implements the tree node methods for a ListPageNode.
    see TreePageNode

---
+++ www/plugins/Premanager/scripts/Premanager/Execution/TreeListPageNode.php
@@ -0,0 +1,76 @@
+<?php
+namespace Premanager\Execution;
+
+use Premanager\Models\StructureNode;
+use Premanager\Execution\Page;
+use Premanager\Execution\StructurePageNode;
+use Premanager\Execution\PageNode;
+use Premanager\Models\User;
+use Premanager\ArgumentNullException;
+use Premanager\IO\Request;
+use Premanager\Execution\Template;
+use Premanager\ArgumentException;
+use Premanager\IO\Output;
+
+/**
+ * A base for tree page nodes consisting of multiple pages
+ * 
+ * Tree page nodes do not have to extend this class, they can implement the same
+ * methods on their own.
+ */
+abstract class TreeListPageNode extends ListPageNode {
+	/**
+	 * @var Premanager\Models\StructureNode
+	 */
+	private $_structureNode;
+	
+	/**
+	 * Creates a new TreePageNode
+	 * 
+	 * @param Premanager\Execution\PageNode $parent the parent node
+	 * @param Premanager\Models\StructureNode $structureNode the structure node
+	 *   this page node is embedded in
+	 * @param int $pageIndex the requested page index or null to use the GET
+	 *   parameter 'page'
+	 * @param int $itemsPerPage the count of items to view on one page or null to
+	 *   use the option Premanager.itemsPerPage
+	 */
+	public function __construct(PageNode $parent, StructureNode $structureNode,
+		$pageIndex = null, $itemsPerPage = null)
+	{
+		parent::__construct($parent, $pageIndex, $itemsPerPage);
+		$this->_structureNode = $structureNode;
+	}
+	
+	/**
+	 * Gets the name that is used in urls
+	 * 
+	 * @return string
+	 */
+	public function getName() {
+		return $this->_structureNode->name;
+	}
+	
+	/**
+	 * Gets the displayed title that is used when the titles of the parent nodes
+	 * are also displayed
+	 * 
+	 * @return string
+	 */
+	public function getTitle() {
+		return $this->_structureNode->title;
+	}
+	
+	/**
+	 * Checks if this object represents the same page as $other
+	 * 
+	 * @param Premanager\Execution\PageNode $other
+	 */
+	public function equals(PageNode $other) {
+		return $other instanceof TreeListPageNode &&
+			$other->_structureNode == $this->_structureNode; 
+	}	   
+	
+}
+
+?>
--- www/plugins/Premanager/scripts/Premanager/Pages/UsersPage.php
+++ www/plugins/Premanager/scripts/Premanager/Pages/UsersPage.php
@@ -1,6 +1,7 @@
 <?php
 namespace Premanager\Pages;
 
+use Premanager\Execution\TreeListPageNode;
 use Premanager\Models\StructureNode;
 use Premanager\Execution\ListPageNode;
 use Premanager\Execution\PageBlock;
@@ -18,24 +19,7 @@ use Premanager\IO\Output;
 /**
  * A page that shows a list of all users
  */
-class UsersPage extends ListPageNode {
-	/**
-	 * @var Premanager\Models\StructureNode
-	 */
-	private $_structureNode;
-	
-	/**
-	 * Creates a new UsersPage
-	 * 
-	 * @param Premanager\Execution\PageNode $parent the parent node
-	 * @param Premanager\Models\StructureNode $structureNode the structure node
-	 *   this page node is embedded in
-	 */
-	public function __construct(PageNode $parent, StructureNode $structureNode) {
-		parent::__construct($parent);
-		$this->_structureNode = $structureNode;
-	}
-	
+class UsersPage extends TreeListPageNode {	
 	/**
 	 * Gets the child specified by its name
 	 * 
@@ -69,25 +53,6 @@ class UsersPage extends ListPageNode {
 		}
 		return $list;
 	}
-	
-	/**
-	 * Gets the name that is used in urls
-	 * 
-	 * @return string
-	 */
-	public function getName() {
-		return $this->_structureNode->name;
-	}
-	
-	/**
-	 * Gets the displayed title that is used when the titles of the parent nodes
-	 * are also displayed
-	 * 
-	 * @return string
-	 */
-	public function getTitle() {
-		return $this->_structureNode->title;
-	}
 
 	/**
 	 * Performs a call of this page
@@ -110,17 +75,7 @@ class UsersPage extends ListPageNode {
 		
 		$page->appendBlock(PageBlock::createTable($head, $body));
 		Output::select($page);
-	}
-	
-	/**
-	 * Checks if this object represents the same page as $other
-	 * 
-	 * @param Premanager\Execution\PageNode $other
-	 */
-	public function equals(PageNode $other) {
-		return $other instanceof UsersPage &&
-			$other->_structureNode == $this->_structureNode; 
-	}	   
+	} 
 	
 	/**
 	 * Counts the items
