3c113d6d2c23552563d30bbe15a6f0cbeae5224a
Yogu <info@yogularm.de>
2010-11-03 23:48:24 +0100

    Created the new Reponse classes StringResponse and XMLResponse
    The StringResponse can output any data simply stored in a string.
    
    The XMLResponse initializes an XMLWriter and outputs its output data.

---
+++ www/plugins/0/scripts/Premanager/Execution/XMLResponse.php
@@ -0,0 +1,61 @@
+<?php 
+namespace Premanager\Execution;
+
+use Premanager\IO\Request;
+
+/**
+ * Defines a response consting of a xml document
+ */
+class XMLResponse extends Response {
+	private $_statusCode;
+	
+	/**
+	 * The XMLWriter that is used for output. The constructor initializes this
+	 * property.
+	 * 
+	 * @var XMLWriter
+	 */
+	public $writer;
+	
+	/**
+	 * Creates a redirection response
+	 * 
+	 * @param string|null $code the HTTP status code, by default 303 See other
+	 */
+	public function __construct($code = 303) {
+		$this->writer = new \XMLWriter();
+		$this->writer->openMemory();
+		$this->writer->startDocument('1.0', 'utf-8');
+		$this->_statusCode = $code;
+	}
+	
+	/**
+	 * Ends the document and gets the content to be sent
+	 * 
+	 * @return string
+	 */
+	public function getContent() {
+		$this->writer->endDocument();
+		return $this->writer->outputMemory(false);
+	}
+	
+	/**
+	 * Gets the MIME type of this response
+	 * 
+	 * @return string
+	 */
+	public function getContentType() {
+		return 'text/xml';
+	}
+	
+	/**
+	 * Gets the HTML status code to be sent (e.g. 200 for OK)
+	 * 
+	 * @return int
+	 */
+	public function getStatusCode() {
+		return $this->_statusCode;
+	}
+}
+
+?>
