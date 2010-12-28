a16f947bc6b2031c0da7d402d8dd6ccc686bf208
unknown <Jan@.(none)>
2010-12-28 13:08:02 +0100

    Improved the display method of checkboxes using the Dwoo formElement plugin

--- www/plugins/0/thirdparty/dwoo/plugins/personal/formElement.php
+++ www/plugins/0/thirdparty/dwoo/plugins/personal/formElement.php
@@ -1,6 +1,8 @@
 <?php
 
-function Dwoo_Plugin_formElement_compile(Dwoo_Compiler $compiler, $name, $label, $description = '', $type = '') {
+function Dwoo_Plugin_formElement_compile(Dwoo_Compiler $compiler, $name, $label,
+	$description = '', $type = '')
+{
 	$error = "\$this->scope['errors'][$name]";
 	$value = "htmlspecialchars(\$this->scope['values'][$name])";
 	$attributes = "'name=\"'.".$name.".'\" id=\"form-'.".$name.".'\"'.".
@@ -10,16 +12,23 @@ function Dwoo_Plugin_formElement_compile(Dwoo_Compiler $compiler, $name, $label,
 		case "'textarea'":
 			$element = "'<textarea '.$attributes.'>'.$value.'</textarea>'";
 			break;
+		case "'checkbox'":
+			$element =
+				"'<label for=\"form-'.$name.'\">'.".
+				"'<input '.$attributes.' type=\"checkbox\"'.".
+				"($value ? ' checked=\"checked\"' : '').' />'.$label.'</label>'";
+			$hideDT = true;
+			break;
 		default:
 			$element = "'<input '.$attributes.' type=\"'.$type.'\" ".
 				"value=\"'.$value.'\" />'";
-	} 
+	}
 	
 	return
-		"'<dl><dt><label for=\"form-'.$name.'\">'.".
+		"'<dl>'.".(!$hideDT ? "'<dt><label for=\"form-'.$name.'\">'.".
 		"Premanager\Execution\Translation::defaultGet('Premanager', 'label', ".
 		"array('label' => $label)).".
-		"'</label></dt><dd>'.$element.".
+		"'</label></dt>'." : '')."'<dd>'.$element.".
 		($description ? "'<p>'.$description.'</p>'." : '').
 		"($error ? (".
 			"'<ul class=\"input-errors\"><li>'.implode('</li><li>', $error).".
