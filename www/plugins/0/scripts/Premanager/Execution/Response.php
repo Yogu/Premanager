f664cdc96e4dc8b4ab57ebbe73162d3555124905
unknown <Jan@.(none)>
2011-01-13 20:08:23 +0100

    Premanager\Execution\Response::send() used a method for sending HTTP status codes that is not supported in a test system.
    Formerly, the string 'x' was used for $string for sending the status code to not send any header line except the status code.
    
    http://square7.ch throws a 505 if the function header() is called with an invalild value for $string. Therefore, the former method is not supported by all servers and had to be corrected.
    
    Now, the status code string is stored in an array and the HTTP status string is created manually.

--- www/plugins/0/scripts/Premanager/Execution/Response.php
+++ www/plugins/0/scripts/Premanager/Execution/Response.php
@@ -1,6 +1,7 @@
 <?php 
 namespace Premanager\Execution;
 
+use Premanager\Debug\Debug;
 use Premanager\Module;
 use Premanager\Strings;
 
@@ -8,6 +9,65 @@ use Premanager\Strings;
  * Defines an object that provides the data for a html response
  */
 abstract class Response extends Module {
+	private static $_statusPhrases = array( 
+		 100 => 'Continue',
+		 101 => 'Switching Protocols',
+		 102 => 'Processing',
+		 200 => 'OK',
+		 201 => 'Created',
+		 202 => 'Accepted',
+		 203 => 'Non-Authoritative Information',
+		 204 => 'No Content',
+		 205 => 'Reset Content',
+		 206 => 'Partial Content',
+		 207 => 'Multi-Status',
+		 300 => 'Multiple Choices',
+		 301 => 'Moved Permanently',
+		 302 => 'Found',
+		 303 => 'See Other',
+		 304 => 'Not Modified',
+		 305 => 'Use Proxy',
+		 306 => 'unused',
+		 307 => 'Temporary Redirect',
+		 400 => 'Bad Request',
+		 401 => 'Authorization Required',
+		 402 => 'Payment Required',
+		 403 => 'Forbidden',
+		 404 => 'Not Found',
+		 405 => 'Method Not Allowed',
+		 406 => 'Not Acceptable',
+		 407 => 'Proxy Authentication Required',
+		 408 => 'Request Time-out',
+		 409 => 'Conflict',
+		 410 => 'Gone',
+		 411 => 'Length Required',
+		 412 => 'Precondition Failed',
+		 413 => 'Request Entity Too Large',
+		 414 => 'Request-URI Too Large',
+		 415 => 'Unsupported Media Type',
+		 416 => 'Requested Range Not Satisfiable',
+		 417 => 'Expectation Failed',
+		 418 => 'unused',
+		 419 => 'unused',
+		 420 => 'unused',
+		 421 => 'unused',
+		 422 => 'Unprocessable Entity',
+		 423 => 'Locked',
+		 424 => 'Failed Dependency',
+		 425 => 'No code',
+		 426 => 'Upgrade Required',
+		 500 => 'Internal Server Error',
+		 501 => 'Method Not Implemented',
+		 502 => 'Bad Gateway',
+		 503 => 'Service Temporarily Unavailable',
+		 504 => 'Gateway Time-out',
+		 505 => 'HTTP Version Not Supported',
+		 506 => 'Variant Also Negotiates',
+		 507 => 'Insufficient Storage',
+		 508 => 'unused',
+		 509 => 'unused',
+		 510 => 'Not Extended');
+	
 	/**
 	 * Gets the content to be sent
 	 * 
@@ -38,7 +98,9 @@ abstract class Response extends Module {
 	 */
 	public function send() {
 		// Let php generate the html status code phrase
-		header('x', true, $this->getStatusCode());
+		$code = $this->getStatusCode();
+		header('HTTP/1.1 '.$code.' '.self::$_statusPhrases[$code], true,
+			$code);
 		
 		$contentType = $this->getContentType();
 		if (Strings::substring($contentType, 0, 4) == 'text')
