80ca3a027388b57f94d24f452f8f9ccd3605ff96
Yogu <info@yogularm.de>
2011-01-15 00:57:54 +0100

    Structure admin: Move pages
    Inclusive all possible error checks and error messages.

---
+++ www/plugins/0/scripts/Premanager/Pages/MoveStructureNodePage.php
@@ -0,0 +1,142 @@
+<?php
+namespace Premanager\Pages;
+
+use Premanager\Models\StructureNodeType;
+use Premanager\Types;
+use Premanager\Models\Right;
+use Premanager\Execution\Rights;
+use Premanager\Execution\Redirection;
+use Premanager\Execution\ToolBarItem;
+use Premanager\Models\Project;
+use Premanager\Premanager;
+use Premanager\Execution\TreeListPageNode;
+use Premanager\Execution\PageBlock;
+use Premanager\Execution\Translation;
+use Premanager\Execution\Page;
+use Premanager\Execution\StructurePageNode;
+use Premanager\Execution\PageNode;
+use Premanager\Models\StructureNode;
+use Premanager\ArgumentNullException;
+use Premanager\IO\Request;
+use Premanager\Execution\Template;
+use Premanager\Execution\ListPageNode;
+use Premanager\ArgumentException;
+use Premanager\IO\Output;
+
+/**
+ * A page that allows to move a structure node into another node
+ */
+class MoveStructureNodePage extends PageNode {	
+	private $_structureNode;
+
+	// ===========================================================================
+	
+	/**
+	 * Creates a new DeleteStructureNodePage
+	 * 
+	 * @param Premanager\Execution\ParentNode $parent the parent node
+	 * @param Premanager\Models\StructureNode $structureNode the structure node to
+	 *   delete
+	 */
+	public function __construct($parent, StructureNode $structureNode) {
+		if (!$structureNode->getParent())
+			throw new ArgumentException('Root page node cannot be moved');
+		
+		parent::__construct($parent);
+
+		$this->_structureNode = $structureNode;
+	} 
+
+	// ===========================================================================
+	
+	/**
+	 * Gets the name that is used in urls
+	 * 
+	 * @return string
+	 */
+	public function getName() {
+		return '+move';
+	}
+	
+	/**
+	 * Gets the displayed title that is used when the titles of the parent nodes
+	 * are also displayed
+	 * 
+	 * @return string
+	 */
+	public function getTitle() {
+		return Translation::defaultGet('Premanager', 'moveNode');
+	}
+	
+	/**
+	 * Performs a call of this page and creates the response object
+	 * 
+	 * @return Premanager\Execution\Response the response object to send
+	 */
+	public function getResponse() {
+		foreach (Request::getPOSTValues() as $key => $value) {
+			if (substr($key, 0, 10) == 'move-into-') {
+				$id = substr($key, 10);
+				break;
+			}
+		}
+		if (Types::isInteger($id) && $id > 0)
+			$selectedNode = StructureNode::getByID($id);
+		if ($selectedNode) {
+			if ($selectedNode == $this->_structureNode->getParent())
+				$error = Translation::defaultGet('Premanager',
+					'moveTargetNotChangedError');
+			else if ($selectedNode == $this->_structureNode ||
+				$selectedNode->isChildOf($this->_structureNode))
+				$error = Translation::defaultGet('Premanager',
+					'moveTargetIsChildError');
+			else if ($selectedNode->getType() == StructureNodeType::TREE)
+				$error = Translation::defaultGet('Premanager', 'moveIntoTreeNodeError');
+			else if (!$selectedNode->areNamesAvailable($this->_structureNode))
+				$error = Translation::defaultGet('Premanager',
+					'nodeNameAlreadyExistsInTargetError');
+			else {
+				if (!Rights::requireRight(
+					Right::getByName('Premanager', 'structureAdmin'), $this->getProject(),
+					$errorResponse))
+					return $errorResponse;
+				
+				$this->_structureNode->setParent($selectedNode);
+				$url = $this->_structureNode->getURL();
+				$url = PageNode::getTreeURL('Premanager', 'structure') . '/-' .
+					($url ? '/' . $url : '');
+				return new Redirection($url);
+			}
+		} else
+			$selectedNode = $this->_structureNode->getParent(); 
+		
+		$page = new Page($this);
+		
+		$page->createMainBlock(
+			'<p>'.Translation::defaultGet('Premanager', 'moveNodeMessage',
+				array('title' => $this->_structureNode->getTitle())).'</p>'.
+			($error ? '<ul class="input-errors"><li>'.$error.'</li></ul>' : ''));
+		
+		$template = new Template('Premanager', 'structureNodeMove');
+		$template->set('node', $this);
+		$template->set('structureNode', $this->_structureNode);
+		$template->set('rootNode', $this->getProject()->getRootNode());
+		$template->set('selectedNode', $selectedNode);
+		$page->appendBlock(PageBlock::createSimple(
+			Translation::defaultGet('Premanager', 'moveNodeTitle'),
+			$template->get()));
+		return $page;
+	}
+	
+	/**
+	 * Checks if this object represents the same page as $other
+	 * 
+	 * @param Premanager\Execution\PageNode $other
+	 */
+	public function equals(PageNode $other) {
+		return $other instanceof MoveStructureNodePage &&
+			$other->_structureNode == $this->_structureNode; 
+	}	    
+}
+
+?>
---
+++ www/plugins/0/templates/structureNodeMoveItem.tpl
@@ -0,0 +1,49 @@
+<li{if $selected==$node} class="selected"{/if}{if $node==$structureNode} class="active"{/if}>
+	<div>
+		<a href="./{treeURL Premanager structure}/{$url}">{html $node->getTitle()}</a>
+		
+		<ul class="toolbar show-titles">
+			{if $node != $structureNode}
+				{if $node == $structureNode->getParent()}
+					{$error = 'moveTargetNotChangedError'}
+				{elseif $node->isChildOf($structureNode)}
+					{$error = 'moveTargetIsChildError'}
+				{elseif $node->getTreeClass()}
+					{$error = 'moveIntoTreeNodeError'}
+				{elseif (!$node->areNamesAvailable($structureNode))}
+					{$error = 'nodeNameAlreadyExistsInTargetError'}
+				{else}
+					{$error = false;}
+				{/if}
+			
+				{if $error}
+					{toolbarItem
+						title=string(Premanager insertNodeHere)
+						description=string(Premanager $error)
+						url=$pageNode->getURL()
+						iconURL='Premanager/images/tools/move.png'
+						enabled=false
+					}
+				{else}
+					{toolbarItem
+						title=string(Premanager insertNodeHere)
+						description=string(Premanager insertNodeHereDescription)
+						url=$pageNode->getURL()
+						iconURL='Premanager/images/tools/move.png'
+						method=POST
+						postName=concat('move-into-', $node->getID())
+					}
+				{/if}
+			{/if}
+		</ul>
+	</div>
+	{$children = $node->getChildren()}
+	{if count($children)}
+		<ul>
+			{foreach $children child}
+				{$name = url($child->getName())}
+				{include file='structureNodeMoveItem.tpl' node=$child pageNode=$pageNode selected=$selected url=concat(concat($url, '/'), $name)}
+			{/foreach}
+		</ul>
+	{/if}
+</li>
